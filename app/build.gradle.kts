6ebb8210507df2dc2b58397f431a94ce6428bfef
Eminis <eminis.ru@gmail.com>
2024-08-14 23:44:22 +0300

    CVA-02 Created hilt module - CryptoModule

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.cardview)
     implementation(libs.okhttp.logging.interceptor)
     implementation(libs.retrofit)
+    implementation(libs.kotlinx.serialization.json)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
---
+++ app/src/main/java/com/example/cryptoviewerapp/di/CryptoModule.kt
@@ -0,0 +1,41 @@
+package com.example.cryptoviewerapp.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import kotlinx.serialization.json.Json
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+class CryptoModule {
+
+    @Provides
+    fun provideHttpClient(): OkHttpClient {
+        val logInterceptor = HttpLoggingInterceptor()
+        logInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        return OkHttpClient.Builder().addInterceptor(logInterceptor).build()
+    }
+
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        val contentType = "application/json".toMediaType()
+        val json = Json { ignoreUnknownKeys = true }
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.coingecko.com/api/v3/")
+            .client(okHttpClient)
+            .addConverterFactory(Json.asConverterFactory(contentType))
+            .build()
+        return retrofit
+    }
+
+    @Provides
+    fun provideRecipeApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+}
