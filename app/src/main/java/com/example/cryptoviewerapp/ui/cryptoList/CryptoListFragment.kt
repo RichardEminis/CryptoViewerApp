43160b60b7673e467eb96761468ad017fcb185fb
Eminis <eminis.ru@gmail.com>
2024-08-17 00:26:08 +0300

    CVA-04 Implemented fonts and fragment_crypto_list

---
+++ app/src/main/java/com/example/cryptoviewerapp/ui/cryptoList/CryptoListFragment.kt
@@ -0,0 +1,26 @@
+package com.example.cryptoviewerapp.ui.cryptoList
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.cryptoviewerapp.databinding.FragmentCryptoListBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CryptoListFragment : Fragment() {
+
+    private var _binding: FragmentCryptoListBinding? = null
+    private val binding
+        get() = _binding
+            ?: throw IllegalStateException("Binding for ActivityMainBinding must not be null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCryptoListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
